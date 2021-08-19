4959425f3875dbc01afd0e0cd35edbeade178d30
unknown <ouafirnazha@gmail.Com>
2021-08-19 23:23:55 +0200

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+     <v-app app>
+            <v-container>
+                <v-btn color="primary">
+                    Primary
+                </v-btn>
+                <v-btn color="secondary">
+                    Secondary
+                </v-btn>
+                <v-btn color="error">
+                    Error
+                </v-btn>
+            </v-container>
+        </v-app> </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
